0be9aae57a61baa3cac3eb217c02971ba2c07856
Daksh Nimavat <dakshnimavat123456@gmail.com>
2025-10-05 20:36:59 +0530

    changed the repop name

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
-// If deploying to GitHub Pages under a repository named "react-techzonix",
-// Vite needs a base path like "/react-techzonix/" so assets resolve correctly.
-// You can change this if your repository name differs.
 export default defineConfig({
-  base: '/react-techzonix/',
+  base: '/techzonix/',
   plugins: [
     tailwindcss(),
   ],
